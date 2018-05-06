9bb330bba733eebce4171302d08f1225eca75d16
coypu <coypu@sdf.org>
2018-05-06 11:44:51 +0300

    Make the linux behaviour the fallback case
    used by all non-OSX non-Windows, as opposite to malfunctioning.

--- src/runner.rs
+++ src/runner.rs
@@ -217,20 +217,6 @@ fn find_binary(name: &str) -> Option<PathBuf> {
         })
 }
 
-#[cfg(target_os = "linux")]
-pub mod platform {
-    use super::find_binary;
-    use std::path::PathBuf;
-
-    pub fn firefox_default_path() -> Option<PathBuf> {
-        find_binary("firefox")
-    }
-
-    pub fn arg_prefix_char(c: char) -> bool {
-        c == '-'
-    }
-}
-
 #[cfg(target_os = "macos")]
 pub mod platform {
     use super::find_binary;
@@ -316,12 +302,13 @@ pub mod platform {
     }
 }
 
-#[cfg(not(any(target_os = "linux", target_os = "macos", target_os = "windows")))]
+#[cfg(not(any(target_os = "macos", target_os = "windows")))]
 pub mod platform {
+    use super::find_binary;
     use std::path::PathBuf;
 
     pub fn firefox_default_path() -> Option<PathBuf> {
-        None
+        find_binary("firefox")
     }
 
     pub fn arg_prefix_char(c: char) -> bool {
